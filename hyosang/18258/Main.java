76549a59503767d975e81c263d77702eb092daf8
HYOSANG <lm668669@gmail.com>
2025-08-25 19:51:49 +0900

    Solve/18258

---
+++ hyosang/18258/Main.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < N; i++) {
+            String command = br.readLine();
+
+            if (command.startsWith("push")) {
+                int value = Integer.parseInt(command.split(" ")[1]);
+                queue.add(value);
+            } else if (command.equals("pop")) {
+                sb.append(queue.isEmpty() ? -1 : queue.poll()).append('\n');
+            } else if (command.equals("size")) {
+                sb.append(queue.size()).append('\n');
+            } else if (command.equals("empty")) {
+                sb.append(queue.isEmpty() ? 1 : 0).append('\n');
+            } else if (command.equals("front")) {
+                sb.append(queue.isEmpty() ? -1 : queue.peek()).append('\n');
+            } else if (command.equals("back")) {
+                sb.append(queue.isEmpty() ? -1 : queue.peekLast()).append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
